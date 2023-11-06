b8461b4359564d54b80c6bc0982a2bfdd556486e
seymamece <seyma90.sk@gmail.com>
2023-11-06 13:13:59 +0300

    first

--- Project-503-Capstone-Project(Django)-on-AWS-Environment/userdata.sh
+++ Project-503-Capstone-Project(Django)-on-AWS-Environment/userdata.sh
@@ -3,13 +3,13 @@ apt-get update -y
 apt-get install git -y
 apt-get install python3 -y
 cd /home/ubuntu/
-TOKEN="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"     # this token is no longer active
-git clone https://$TOKEN@github.com/rumeysakdogan/my-aws-capstone-project
-cd /home/ubuntu/my-aws-capstone-project
+TOKEN="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"    
+git clone https://$TOKEN@https://github.com/seymamece/Aws_Capstone_Project.git
+cd /home/ubuntu/Aws_Capstone_Project
 apt install python3-pip -y
 apt-get install python3.7-dev libmysqlclient-dev -y
 pip3 install -r requirements.txt
-cd /home/ubuntu/my-aws-capstone-project/src
+cd /home/ubuntu/Aws_Capstone_Project/src
 python3 manage.py collectstatic --noinput
 python3 manage.py makemigrations
 python3 manage.py migrate
