061ca0518d3ead5f321a925e1def65f98baa61e9
seymamece <seyma90.sk@gmail.com>
2023-11-08 12:08:08 +0300

    update

--- Project-503-Capstone-Project(Django)-on-AWS-Environment/userdata.sh
+++ Project-503-Capstone-Project(Django)-on-AWS-Environment/userdata.sh
@@ -1,16 +1,18 @@
-#!/bin/bash
-apt-get update -y
-apt-get install git -y
-apt-get install python3 -y
+#! /bin/bash
+apt update -y
+apt upgrade -y
+apt install -y python3 python3-pip python3-venv
 cd /home/ubuntu/
-TOKEN="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"    
+TOKEN="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"
 git clone https://$TOKEN@https://github.com/seymamece/Aws_Capstone_Project.git
 cd /home/ubuntu/Aws_Capstone_Project
-apt install python3-pip -y
-apt-get install python3.7-dev libmysqlclient-dev -y
+python3 -m venv venv
+source venv/bin/activate
+pip install -r requirements.txt
+apt-get install -y libmysqlclient-dev
 pip3 install -r requirements.txt
-cd /home/ubuntu/Aws_Capstone_Project/src
-python3 manage.py collectstatic --noinput
-python3 manage.py makemigrations
-python3 manage.py migrate
-python3 manage.py runserver 0.0.0.0:80
+cd src/
+python manage.py makemigrations
+python manage.py migrate
+python manage.py collectstatic --noinput
+python manage.py runserver 0.0.0.0:8000
